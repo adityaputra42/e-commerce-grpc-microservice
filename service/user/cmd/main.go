5dc4c6a20ebac9e15093a3ba30b556ec1a80a7bd
adityaputra42 <aditiyaputra42@gmail.com>
2025-06-11 11:58:34 +0700

    add init grpc server in main user service

--- service/user/cmd/main.go
+++ service/user/cmd/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"context"
 	"e-commerce-microservice/user/internal/config"
 	"e-commerce-microservice/user/internal/db"
+	"e-commerce-microservice/user/internal/handler"
+	"e-commerce-microservice/user/internal/pb"
+	"e-commerce-microservice/user/internal/repository"
+	"e-commerce-microservice/user/internal/services"
+	"e-commerce-microservice/user/internal/token"
+	"e-commerce-microservice/user/internal/utils"
+	"errors"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/rs/zerolog/log"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"gorm.io/gorm"
 )
+
+var interruptSignals = []os.Signal{
+	os.Interrupt,
+	syscall.SIGTERM,
+	syscall.SIGINT,
+}
 
 func main() {
 	conf, err := config.LoadConfig(".")
@@ -13,10 +35,73 @@ func main() {
 		log.Fatal().Err(err).Msg("Cannot load config")
 		panic(err)
 	}
+	shutdownCtx, stop := signal.NotifyContext(context.Background(), interruptSignals...)
+	defer stop()
+	waitGroup, ctx := errgroup.WithContext(shutdownCtx)
 
-	_, err = db.InitDB(conf.DbSource)
+	db, err := db.InitDB(conf.DbSource)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Cannot initialize database")
 		panic(err)
 	}
+	runGrpcServer(ctx, waitGroup, conf, db)
+
+	err = waitGroup.Wait()
+	if err != nil {
+		log.Fatal().Err(err).Msg("error from wait group")
+	}
+
+}
+func runGrpcServer(
+	ctx context.Context,
+	waitGroup *errgroup.Group,
+	config config.Configuration,
+	db *gorm.DB,
+
+) {
+
+	tokenMaker, err := token.NewJWTMaker(config.SecretKey)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot create token maker")
+
+	}
+	userRepo := repository.NewUserRepositoryImpl(db)
+	userService := services.NewUserService(tokenMaker, db, userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	grpcLogger := grpc.UnaryInterceptor(utils.GrpcLogger)
+	grpcServer := grpc.NewServer(grpcLogger)
+
+	pb.RegisterUserServiceServer(grpcServer, userHandler)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GrpcServerAddress)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot create listener")
+	}
+
+	waitGroup.Go(func() error {
+		log.Info().Msgf("start gRPC server at %s", listener.Addr().String())
+
+		err = grpcServer.Serve(listener)
+		if err != nil {
+			if errors.Is(err, grpc.ErrServerStopped) {
+				return nil
+			}
+			log.Error().Err(err).Msg("gRPC server failed to serve")
+			return err
+		}
+
+		return nil
+	})
+
+	waitGroup.Go(func() error {
+		<-ctx.Done()
+		log.Info().Msg("graceful shutdown gRPC server")
+
+		grpcServer.GracefulStop()
+		log.Info().Msg("gRPC server is stopped")
+
+		return nil
+	})
 }
--- service/user/go.mod
+++ service/user/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/rs/zerolog v1.34.0
 	github.com/spf13/viper v1.20.1
 	golang.org/x/crypto v0.38.0
+	golang.org/x/sync v0.14.0
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250603155806-513f23925822
 	google.golang.org/grpc v1.72.2
 	google.golang.org/protobuf v1.36.6
@@ -36,7 +37,6 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/net v0.35.0 // indirect
-	golang.org/x/sync v0.14.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
