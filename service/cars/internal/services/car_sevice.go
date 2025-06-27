2b40498efe67d192f92580d5e376faac792652c1
adityaputra42 <aditiyaputra42@gmail.com>
2025-06-27 21:58:22 +0700

    add car services

--- service/cars/internal/services/car_sevice.go
+++ service/cars/internal/services/car_sevice.go
@@ -88,22 +88,167 @@ func (c *CarServiceImpl) CreateCar(ctx context.Context, req *pb.CreateCarRequest
 
 // DeletCar implements CarService.
 func (c *CarServiceImpl) DeletCar(ctx context.Context, req *pb.DeleteCarRequest) (*pb.DeleteCarResponse, error) {
-	panic("unimplemented")
+	authPayload, err := utils.AuthorizationUser(ctx, c.tokenMaker)
+
+	if err != nil {
+		return &pb.DeleteCarResponse{Message: utils.UnauthenticatedError(err).Error()}, utils.UnauthenticatedError(err)
+	}
+	if authPayload.Role != utils.AdminRole {
+		return &pb.DeleteCarResponse{Message: "user not permited"}, fmt.Errorf("user not permited")
+	}
+
+	result, err := c.repo.FindCarById(ctx, req.GetId())
+
+	if err != nil {
+		return &pb.DeleteCarResponse{Message: "failed to get car"}, fmt.Errorf("failed to get car")
+	}
+	err = c.repo.DeletCar(ctx, result)
+	if err != nil {
+		return &pb.DeleteCarResponse{Message: "failed to delete car"}, fmt.Errorf("failed to delete car")
+	}
+	return &pb.DeleteCarResponse{Message: "Success"}, nil
+
 }
 
 // FindAllCars implements CarService.
 func (c *CarServiceImpl) FindAllCars(ctx context.Context, req *pb.ListCarsRequest) (*pb.ListCarsResponse, error) {
-	panic("unimplemented")
+	var cars pb.ListCarsResponse
+
+	result, err := c.repo.FindAllCars(ctx)
+	if err != nil {
+		return &pb.ListCarsResponse{}, fmt.Errorf("failed to get cars")
+
+	}
+	listCar := []*pb.Car{}
+
+	for _, v := range result {
+		listCar = append(listCar, &pb.Car{
+			Id:           v.ID,
+			Name:         v.Name,
+			Model:        v.Model,
+			Brand:        v.Brand,
+			Year:         v.Year,
+			Mileage:      v.Mileage,
+			Transmission: v.Transmission,
+			FuelType:     v.FuelType,
+			Location:     v.Location,
+			Description:  v.Description,
+			Images:       v.Images,
+			Price:        v.Price,
+			Currency:     v.Currency,
+			IsSold:       v.IsSold,
+			CreatedAt:    timestamppb.New(v.CreatedAt),
+			UpdatedAt:    timestamppb.New(v.UpdatedAt),
+		})
+	}
+	cars = pb.ListCarsResponse{
+		Cars: listCar,
+	}
+	return &cars, nil
+
 }
 
 // FindCarById implements CarService.
 func (c *CarServiceImpl) FindCarById(ctx context.Context, req *pb.GetCarRequest) (*pb.CarResponse, error) {
-	panic("unimplemented")
+	var car pb.CarResponse
+
+	result, err := c.repo.FindCarById(ctx, req.GetId())
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get car")
+	}
+	car = pb.CarResponse{
+		Car: &pb.Car{
+			Id:           result.ID,
+			Name:         result.Name,
+			Model:        result.Model,
+			Brand:        result.Brand,
+			Year:         result.Year,
+			Mileage:      result.Mileage,
+			Transmission: result.Transmission,
+			FuelType:     result.FuelType,
+			Location:     result.Location,
+			Description:  result.Description,
+			Images:       result.Images,
+			Price:        result.Price,
+			Currency:     result.Currency,
+			IsSold:       result.IsSold,
+			CreatedAt:    timestamppb.New(result.CreatedAt),
+			UpdatedAt:    timestamppb.New(result.UpdatedAt),
+		},
+	}
+
+	return &car, nil
+
 }
 
 // UpdateCar implements CarService.
 func (c *CarServiceImpl) UpdateCar(ctx context.Context, req *pb.UpdateCarRequest) (*pb.CarResponse, error) {
-	panic("unimplemented")
+	var car pb.CarResponse
+
+	err := c.db.Transaction(func(tx *gorm.DB) error {
+
+		_, err := utils.AuthorizationUser(ctx, c.tokenMaker)
+
+		if err != nil {
+			return utils.UnauthenticatedError(err)
+		}
+
+		carUpdate, err := c.repo.FindCarById(ctx, req.GetId())
+
+		if err != nil {
+			return fmt.Errorf("failed to get car with id %s", req.Id)
+		}
+
+		request := model.Cars{
+			ID:           carUpdate.ID,
+			Name:         req.GetName(),
+			Brand:        req.GetBrand(),
+			Model:        req.GetModel(),
+			Year:         req.GetYear(),
+			Mileage:      req.GetMileage(),
+			Transmission: req.GetTransmission(),
+			FuelType:     req.GetFuelType(),
+			Location:     req.GetLocation(),
+			Description:  req.GetDescription(),
+			Images:       req.GetImages(),
+			Price:        req.GetPrice(),
+			Currency:     req.GetCurrency(),
+		}
+
+		result, err := c.repo.UpdateCar(ctx, tx, &request)
+
+		if err != nil {
+			return fmt.Errorf("failed to update car")
+		}
+		car = pb.CarResponse{
+			Car: &pb.Car{
+				Id:           result.ID,
+				Name:         result.Name,
+				Model:        result.Model,
+				Brand:        result.Brand,
+				Year:         result.Year,
+				Mileage:      result.Mileage,
+				Transmission: result.Transmission,
+				FuelType:     result.FuelType,
+				Location:     result.Location,
+				Description:  result.Description,
+				Images:       result.Images,
+				Price:        result.Price,
+				Currency:     result.Currency,
+				IsSold:       result.IsSold,
+				CreatedAt:    timestamppb.New(result.CreatedAt),
+				UpdatedAt:    timestamppb.New(result.UpdatedAt),
+			},
+		}
+		return nil
+	})
+
+	if err != nil {
+		return &pb.CarResponse{}, err
+	}
+
+	return &car, nil
 }
 
 func NewCarService(tokenMaker token.Maker,
