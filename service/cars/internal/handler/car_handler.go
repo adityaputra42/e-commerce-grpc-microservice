cc5bd7aaa2ef7fd070011fb66af0166dc115fdd3
adityaputra42 <aditiyaputra42@gmail.com>
2025-06-30 12:36:55 +0700

    add validation car

--- service/cars/internal/handler/car_handler.go
+++ service/cars/internal/handler/car_handler.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"e-commerce-microservice/cars/internal/pb"
 	"e-commerce-microservice/cars/internal/services"
+	"e-commerce-microservice/cars/internal/utils"
+	"e-commerce-microservice/cars/internal/val"
+	"strconv"
 
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -19,6 +23,12 @@ func NewCarHandler(service services.CarService) *CarHandler {
 }
 
 func (c *CarHandler) CreateCar(ctx context.Context, req *pb.CreateCarRequest) (*pb.CarResponse, error) {
+	violations := validateCreateCar(req)
+	if violations != nil {
+		return nil, utils.InvalidArgumentError(violations)
+
+	}
+
 	response, err := c.service.CreateCar(ctx, req)
 	if err != nil {
 		status.Errorf(codes.Internal, "%s", err.Error())
@@ -57,4 +67,43 @@ func (a *CarHandler) FindCarById(ctx context.Context, req *pb.GetCarRequest) (*p
 		status.Errorf(codes.Internal, "%s", err.Error())
 	}
 	return res, nil
+}
+
+func validateCreateCar(req *pb.CreateCarRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+
+	if err := val.ValidateName(req.GetName()); err != nil {
+		violations = append(violations, utils.FieldViolation("name", err))
+	}
+
+	if err := val.ValidateBrand(req.GetBrand()); err != nil {
+		violations = append(violations, utils.FieldViolation("brand", err))
+	}
+
+	if err := val.ValidateModel(req.GetModel()); err != nil {
+		violations = append(violations, utils.FieldViolation("model", err))
+	}
+
+	if err := val.ValidateYear(strconv.Itoa(int(req.GetYear()))); err != nil {
+		violations = append(violations, utils.FieldViolation("year", err))
+	}
+
+	if err := val.ValidateMileage(strconv.Itoa(int(req.GetMileage()))); err != nil {
+		violations = append(violations, utils.FieldViolation("mileage", err))
+	}
+	if err := val.ValidateTransmission(req.GetTransmission()); err != nil {
+		violations = append(violations, utils.FieldViolation("transmission", err))
+	}
+	if err := val.ValidateFullType(req.GetFuelType()); err != nil {
+		violations = append(violations, utils.FieldViolation("fuel_type", err))
+	}
+	if err := val.ValidateLocation(req.GetLocation()); err != nil {
+		violations = append(violations, utils.FieldViolation("location", err))
+	}
+	if err := val.ValidateDescription(req.GetDescription()); err != nil {
+		violations = append(violations, utils.FieldViolation("description", err))
+	}
+	if err := val.ValidateCurrency(req.GetCurrency()); err != nil {
+		violations = append(violations, utils.FieldViolation("currency", err))
+	}
+	return violations
 }
---
+++ service/cars/internal/val/validator.go
@@ -0,0 +1,145 @@
+package val
+
+import (
+	"fmt"
+	"regexp"
+)
+
+const (
+	BNB = "BNB"
+	ETH = "ETH"
+	SOL = "SOL"
+	IDR = "IDR"
+)
+
+var (
+	isValidateName         = regexp.MustCompile(`^[a-zA-Z0-9\s\-]+$`).MatchString
+	isValidateBrand        = regexp.MustCompile(`^[a-zA-Z0-9\s\-]+$`).MatchString
+	isValidateModel        = regexp.MustCompile(`^[a-zA-Z0-9\s\-]+$`).MatchString
+	isValidateYear         = regexp.MustCompile(`^\d{4}$`).MatchString
+	isValidateMileage      = regexp.MustCompile(`^(0|[1-9]\d{0,6})$`).MatchString
+	isValidateTransmission = regexp.MustCompile(`^(?i)(manual|automatic)$`).MatchString
+	isValidateFuelType     = regexp.MustCompile(`^(?i)(petrol|diesel|electric|hybrid)$`).MatchString
+	isValidateLocation     = regexp.MustCompile(`^[a-zA-Z0-9\s,\.-]+$`).MatchString
+	isValidateDescription  = regexp.MustCompile(`^.{10,}$`).MatchString
+)
+
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case BNB, ETH, SOL, IDR:
+		return true
+	}
+	return false
+}
+
+func ValidateString(value string, minLength int, maxlength int) error {
+
+	n := len(value)
+
+	if n < minLength || n > maxlength {
+		return fmt.Errorf("must content from %d-%d characters", minLength, maxlength)
+	}
+
+	return nil
+}
+
+func ValidateName(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidateName(value) {
+		return fmt.Errorf("must contains only letters or spaces")
+	}
+	return nil
+}
+
+func ValidateBrand(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidateBrand(value) {
+		return fmt.Errorf("must contains letters or spaces")
+	}
+	return nil
+}
+
+func ValidateModel(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidateModel(value) {
+		return fmt.Errorf("must contains only letters or spaces")
+	}
+	return nil
+}
+
+func ValidateYear(value string) error {
+
+	if !isValidateYear(value) {
+		return fmt.Errorf("must contains only Number")
+	}
+	return nil
+}
+
+func ValidateMileage(value string) error {
+
+	if !isValidateMileage(value) {
+		return fmt.Errorf("must contains only number")
+	}
+	return nil
+}
+
+func ValidateTransmission(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidateTransmission(value) {
+		return fmt.Errorf("must contains only manual or automatic")
+	}
+	return nil
+}
+
+func ValidateFullType(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidateFuelType(value) {
+		return fmt.Errorf("must only contains one of petrol, diesel, electric, or hybrid.")
+	}
+	return nil
+}
+
+func ValidateLocation(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidateLocation(value) {
+		return fmt.Errorf("must contains only letters or spaces")
+	}
+	return nil
+}
+
+func ValidateDescription(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidateDescription(value) {
+		return fmt.Errorf("must contains only letters or spaces")
+	}
+	return nil
+}
+
+func ValidateCurrency(value string) error {
+
+	if !IsSupportedCurrency(value) {
+		return fmt.Errorf("must only contains one of BNB, ETH, SOL, or IDR.")
+	}
+	return nil
+}
