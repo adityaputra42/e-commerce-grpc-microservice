3d156369663f9984f96c3cced2e6eeba61648b8e
adityaputra42 <aditiyaputra42@gmail.com>
2025-07-04 16:23:51 +0700

    add validation order handler

--- service/order/internal/handler/order_handler.go
+++ service/order/internal/handler/order_handler.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"e-commerce-microservice/order/internal/pb"
 	"e-commerce-microservice/order/internal/services"
+	"e-commerce-microservice/order/internal/utils"
+	"e-commerce-microservice/order/internal/val"
 
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -18,6 +21,11 @@ func NewOrderHandler(service services.OrdeService) *OrderHandler {
 	return &OrderHandler{service: service}
 }
 func (o *OrderHandler) CreateOrder(ctx context.Context, req *pb.CreateOrderRequest) (*pb.OrderResponse, error) {
+	violations := validateCreateOrder(req)
+	if violations != nil {
+		return nil, utils.InvalidArgumentError(violations)
+
+	}
 	res, err := o.service.CreateOrder(ctx, req)
 	if err != nil {
 		status.Errorf(codes.Internal, "%s", err.Error())
@@ -27,7 +35,11 @@ func (o *OrderHandler) CreateOrder(ctx context.Context, req *pb.CreateOrderReque
 }
 
 func (o *OrderHandler) CancelOrder(ctx context.Context, req *pb.CancelOrderRequest) (*pb.OrderResponse, error) {
+	violations := validateId(req.GetId())
+	if violations != nil {
+		return nil, utils.InvalidArgumentError(violations)
 
+	}
 	res, err := o.service.CancelOrder(ctx, req)
 	if err != nil {
 		status.Errorf(codes.Internal, "%s", err.Error())
@@ -36,6 +48,12 @@ func (o *OrderHandler) CancelOrder(ctx context.Context, req *pb.CancelOrderReque
 }
 
 func (o *OrderHandler) DeleteOrder(ctx context.Context, req *pb.DeleteOrderRequest) (*pb.DeleteOrderResponse, error) {
+
+	violations := validateId(req.GetId())
+	if violations != nil {
+		return nil, utils.InvalidArgumentError(violations)
+
+	}
 	res, err := o.service.DeleteOrder(ctx, req)
 	if err != nil {
 		status.Errorf(codes.Internal, "%s", err.Error())
@@ -44,7 +62,11 @@ func (o *OrderHandler) DeleteOrder(ctx context.Context, req *pb.DeleteOrderReque
 }
 
 func (o *OrderHandler) FindAllOrder(ctx context.Context, req *pb.ListOrdersRequest) (*pb.ListOrdersResponse, error) {
+	violations := validateAllOrder(req)
+	if violations != nil {
+		return nil, utils.InvalidArgumentError(violations)
 
+	}
 	res, err := o.service.FindAllOrder(ctx, req)
 	if err != nil {
 		status.Errorf(codes.Internal, "%s", err.Error())
@@ -53,7 +75,11 @@ func (o *OrderHandler) FindAllOrder(ctx context.Context, req *pb.ListOrdersReque
 }
 
 func (o *OrderHandler) FindOrderById(ctx context.Context, req *pb.GetOrderRequest) (*pb.OrderResponse, error) {
+	violations := validateId(req.GetId())
+	if violations != nil {
+		return nil, utils.InvalidArgumentError(violations)
 
+	}
 	res, err := o.service.FindOrderById(ctx, req)
 	if err != nil {
 		status.Errorf(codes.Internal, "%s", err.Error())
@@ -62,10 +88,44 @@ func (o *OrderHandler) FindOrderById(ctx context.Context, req *pb.GetOrderReques
 }
 
 func (o *OrderHandler) UpdateOrder(ctx context.Context, req *pb.UpdateOrderRequest) (*pb.OrderResponse, error) {
+	violations := validateId(req.GetId())
+	if violations != nil {
+		return nil, utils.InvalidArgumentError(violations)
 
+	}
 	res, err := o.service.UpdateOrder(ctx, req)
 	if err != nil {
 		status.Errorf(codes.Internal, "%s", err.Error())
 	}
 	return res, nil
+}
+
+func validateCreateOrder(req *pb.CreateOrderRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, utils.FieldViolation("username", err))
+	}
+	if err := val.ValidateUUID(req.GetCarId()); err != nil {
+		violations = append(violations, utils.FieldViolation("car_id", err))
+	}
+
+	return violations
+}
+
+func validateId(id string) (violations []*errdetails.BadRequest_FieldViolation) {
+
+	if err := val.ValidateUUID(id); err != nil {
+		violations = append(violations, utils.FieldViolation("id", err))
+	}
+
+	return violations
+}
+
+func validateAllOrder(req *pb.ListOrdersRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, utils.FieldViolation("username", err))
+	}
+
+	return violations
 }
--- service/order/internal/val/validator.go
+++ service/order/internal/val/validator.go
@@ -3,34 +3,13 @@ package val
 import (
 	"fmt"
 	"regexp"
-)
 
-const (
-	BNB = "BNB"
-	ETH = "ETH"
-	SOL = "SOL"
-	IDR = "IDR"
+	"github.com/google/uuid"
 )
 
 var (
-	isValidateName         = regexp.MustCompile(`^[a-zA-Z0-9\s\-]+$`).MatchString
-	isValidateBrand        = regexp.MustCompile(`^[a-zA-Z0-9\s\-]+$`).MatchString
-	isValidateModel        = regexp.MustCompile(`^[a-zA-Z0-9\s\-]+$`).MatchString
-	isValidateYear         = regexp.MustCompile(`^\d{4}$`).MatchString
-	isValidateMileage      = regexp.MustCompile(`^(0|[1-9]\d{0,6})$`).MatchString
-	isValidateTransmission = regexp.MustCompile(`^(?i)(manual|automatic)$`).MatchString
-	isValidateFuelType     = regexp.MustCompile(`^(?i)(petrol|diesel|electric|hybrid)$`).MatchString
-	isValidateLocation     = regexp.MustCompile(`^[a-zA-Z0-9\s,\.-]+$`).MatchString
-	isValidateDescription  = regexp.MustCompile(`^.{10,}$`).MatchString
+	isValidateUsername = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString
 )
-
-func IsSupportedCurrency(currency string) bool {
-	switch currency {
-	case BNB, ETH, SOL, IDR:
-		return true
-	}
-	return false
-}
 
 func ValidateString(value string, minLength int, maxlength int) error {
 
@@ -43,103 +22,22 @@ func ValidateString(value string, minLength int, maxlength int) error {
 	return nil
 }
 
-func ValidateName(value string) error {
+func ValidateUsername(value string) error {
 	if err := ValidateString(value, 3, 100); err != nil {
 		return err
 	}
 
-	if !isValidateName(value) {
-		return fmt.Errorf("must contains only letters or spaces")
+	if !isValidateUsername(value) {
+		return fmt.Errorf("must contains only lowercase letters, digit or underscore")
 	}
 	return nil
 }
 
-func ValidateBrand(value string) error {
-	if err := ValidateString(value, 3, 100); err != nil {
-		return err
-	}
+func ValidateUUID(value string) error {
+	_, err := uuid.Parse(value)
 
-	if !isValidateBrand(value) {
-		return fmt.Errorf("must contains letters or spaces")
-	}
-	return nil
-}
-
-func ValidateModel(value string) error {
-	if err := ValidateString(value, 3, 100); err != nil {
-		return err
-	}
-
-	if !isValidateModel(value) {
-		return fmt.Errorf("must contains only letters or spaces")
-	}
-	return nil
-}
-
-func ValidateYear(value string) error {
-
-	if !isValidateYear(value) {
-		return fmt.Errorf("must contains only Number")
-	}
-	return nil
-}
-
-func ValidateMileage(value string) error {
-
-	if !isValidateMileage(value) {
-		return fmt.Errorf("must contains only number")
-	}
-	return nil
-}
-
-func ValidateTransmission(value string) error {
-	if err := ValidateString(value, 3, 100); err != nil {
-		return err
-	}
-
-	if !isValidateTransmission(value) {
-		return fmt.Errorf("must contains only manual or automatic")
-	}
-	return nil
-}
-
-func ValidateFullType(value string) error {
-	if err := ValidateString(value, 3, 100); err != nil {
-		return err
-	}
-
-	if !isValidateFuelType(value) {
-		return fmt.Errorf("must only contains one of petrol, diesel, electric, or hybrid.")
-	}
-	return nil
-}
-
-func ValidateLocation(value string) error {
-	if err := ValidateString(value, 3, 100); err != nil {
-		return err
-	}
-
-	if !isValidateLocation(value) {
-		return fmt.Errorf("must contains only letters or spaces")
-	}
-	return nil
-}
-
-func ValidateDescription(value string) error {
-	if err := ValidateString(value, 3, 100); err != nil {
-		return err
-	}
-
-	if !isValidateDescription(value) {
-		return fmt.Errorf("must contains only letters or spaces")
-	}
-	return nil
-}
-
-func ValidateCurrency(value string) error {
-
-	if !IsSupportedCurrency(value) {
-		return fmt.Errorf("must only contains one of BNB, ETH, SOL, or IDR.")
+	if err != nil {
+		return fmt.Errorf("invalid id")
 	}
 	return nil
 }
